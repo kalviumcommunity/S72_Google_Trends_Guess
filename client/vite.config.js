8b82903fe6a7f34d06f53e7380147b096d4695b8
Gaurav-205 <gauravkhandelwal205@gmail.com>
2025-05-01 00:10:39 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,24 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'esbuild',
-    target: 'es2015',
-    sourcemap: true,
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    cssCodeSplit: true,
-    modulePreload: true,
-    cssMinify: true
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-    esbuildOptions: {
-      target: 'es2015'
-    }
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  }
 })
